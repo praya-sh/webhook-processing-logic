21a4d20919ee7e58ba312463596445ce912b6749
Prayash Shakya <prayashshakya20@gmail.com>
2025-09-20 19:33:21 +0545

    send webhook

--- test_webhook.py
+++ test_webhook.py
@@ -13,4 +13,11 @@ def generate_signature(payload: str) -> str:
     return hmac.new(SECRET.encode(), payload.encode(), hashlib.sha256).hexdigest()
 
 def send_webhook(payload: str, content_type="application/json", sig= True):
-    
+    headers = {"Content-Type": content_type}
+    if sig:
+        signature = generate_signature(payload)
+        headers["X-signature"] = f"sha256={signature}"
+    response = requests.post(WEBHOOK_URL, data=payload, headers=headers)
+    print(f"Payload ({content_type}):\n{payload}")
+    print(f"Status Code: {response.status_code}\nResponse: {response.text}")
+    return response
