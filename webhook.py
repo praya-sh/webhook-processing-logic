c2e63fc6299a3f7f1fefb879150d35e0bdaca92a
Prayash Shakya <prayashshakya20@gmail.com>
2025-09-20 19:00:09 +0545

    signature verification

--- webhook.py
+++ webhook.py
@@ -12,5 +12,15 @@ processed_events = set()
 
 @app.route("/webhook/camera", methods=["POST"])
 
+def webhook():
+    raw_data = request.data.decode("utf-8")
+    received_sig = request.headers.get("X-signature")
+    if not received_sig:
+        return jsonify({"error": "Missing Signature"}), 401
+    
+    expected_sig = "sha256=" + hmac.new(
+        SECRET.encode(), raw_data.encode(),hashlib.sha256
+    ).hexdigest()
 
+    
     
